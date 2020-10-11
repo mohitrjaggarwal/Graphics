0ae945c317fadf91e4e9030d5b5865619fd4fb18
mohitrjaggarwal <jssmohitrjaggarwal@gmail.com>
2020-10-11 14:22:59 +0530

    change before pushing

--- 3d.py
+++ 3d.py
@@ -1,11 +1,5 @@
-import numpy as np
-from mpl_toolkits.mplot3d import axes3d 
-import matplotlib.pyplot as plt
-
-fig = plt.figure()
-ax = fig.add_subplot(111, projection = '3d')
-x = [1,2,3,4,5,6,7,8,9]
-  this is used for 2nd branching
+i started with pulling repository,with git and then made changes ->branching-> merging/rebasing and generating sshkey.
+at last but not least -> pushing it to central repository!!!
 
 
 
